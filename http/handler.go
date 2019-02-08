941115790e96b69be508bc4456d2fef076f89eea
Lufty <lufty.abdillah@gmail.com>
2019-02-09 00:04:50 +0700

    fixed response copying

--- http/handler.go
+++ http/handler.go
@@ -3,7 +3,7 @@ package http
 import (
 	"encoding/base64"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -20,6 +20,7 @@ func HandleRequest(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// Proxy authentication
 	credential := fmt.Sprintf("%s:%s", config.ProxyUsername, config.ProxyPassword)
 	authorization := base64.StdEncoding.EncodeToString([]byte(credential))
 	request.Header.Add("Proxy-Authorization", fmt.Sprintf("Basic %s", authorization))
@@ -30,6 +31,7 @@ func HandleRequest(res http.ResponseWriter, req *http.Request) {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	client := &http.Client{
 		Transport: &http.Transport{
 			Proxy: http.ProxyURL(proxyURL),
@@ -42,20 +44,17 @@ func HandleRequest(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
-	content, err := ioutil.ReadAll(resp.Body)
+
+	for hkey, hval := range resp.Header {
+		for _, v := range hval {
+			res.Header().Add(hkey, v)
+		}
+	}
+	res.WriteHeader(resp.StatusCode)
+	_, err = io.Copy(res, resp.Body)
 	if err != nil {
 		log.Println(err)
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
-	}
-	length, err := res.Write(content)
-	if err != nil {
-		log.Println(err)
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if length != len(content) {
-		log.Println("Proxy response incompletely written")
-		http.Error(res, "Failed to forward response", http.StatusInternalServerError)
 	}
 }
