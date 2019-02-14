ea7596cd4ce87d6a907f62f3787238f0efbfaec3
Lufty <lufty.abdillah@gmail.com>
2019-02-14 21:38:54 +0700

    simplified handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,127 +1,86 @@
 package handler
 
 import (
-	"encoding/base64"
 	"io"
-	"log"
 	"net/http"
-	"net/http/httputil"
-	"strings"
 
 	"github.com/arknable/fwdproxy/config"
+	mylog "github.com/arknable/fwdproxy/log"
 	"github.com/arknable/fwdproxy/server"
 	"github.com/arknable/fwdproxy/user"
+	log "github.com/sirupsen/logrus"
 )
 
 // HandleRequest handles requests
 func HandleRequest(res http.ResponseWriter, req *http.Request) {
-	var (
-		fwdMethod string
-		username  string
-		password  string
-		data      []byte
-		err       error
-	)
-
-	// Dump request for log information
-	data, err = httputil.DumpRequest(req, true)
-	if err != nil {
-		log.Println("httputil.DumpRequest: ", err)
-	} else {
-		log.Printf("Request from client: \n%s\n", string(data))
-	}
-
-	authOK := false
-	if req.Method == http.MethodConnect { // HTTPS
-		fwdMethod = http.MethodGet
-		header := req.Header.Get("Proxy-Authorization")
-		if len(header) > 0 {
-			parts := strings.Split(header, " ")
-			data, err = base64.StdEncoding.DecodeString(parts[1])
-			if err != nil {
-				log.Println("base64.StdEncoding.Decode: ", err)
-				http.Error(res, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			parts = strings.Split(string(data), ":")
-			authOK = true
-			username = parts[0]
-			password = parts[1]
-		}
-		req.URL.Scheme = "https" // Somehow scheme always cleared
-	} else {
-		fwdMethod = req.Method
-		username, password, authOK = req.BasicAuth()
-	}
-
-	if !authOK {
-		log.Println("Unauthorized access, missing authentication")
-		http.Error(res, "Restricted access only", http.StatusUnauthorized)
+	username, password, ok := req.BasicAuth()
+	if !ok {
+		msg := "Unauthorized request"
+		http.Error(res, msg, http.StatusUnauthorized)
+		mylog.WithRequest(req).Warning(msg)
 		return
 	}
+	mylog.WithRequest(req).Info("Incoming request")
 
+	credFields := log.Fields{
+		"username": username,
+		"pasword":  password,
+	}
 	valid, err := user.Repo().Validate(username, password)
 	if err != nil {
-		http.Error(res, "Failed to validate user", http.StatusInternalServerError)
-		log.Println(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		log.WithFields(credFields).Error(err)
 		return
 	}
 	if !valid {
-		log.Printf("Invalid authentication: %s/%s\n", username, password)
-		http.Error(res, "You have no access to do a request", http.StatusForbidden)
+		msg := "Forbidden request"
+		http.Error(res, msg, http.StatusForbidden)
+		log.WithFields(credFields).Warning(msg)
 		return
 	}
+	log.WithFields(credFields).Info("Authenticated")
 
-	request, err := http.NewRequest(fwdMethod, req.URL.String(), req.Body)
+	method := req.Method
+	if req.TLS != nil {
+		method = http.MethodGet
+	}
+	request, err := http.NewRequest(method, req.URL.String(), req.Body)
 	if err != nil {
-		log.Println("http.NewRequest: ", err)
 		http.Error(res, err.Error(), http.StatusInternalServerError)
+		log.Error(err)
 		return
 	}
 	request.Header.Set("Host", req.Host)
-	request.Header.Set("X-Forwarded-For", req.RemoteAddr)
-	for hkey, hval := range req.Header {
-		for _, v := range hval {
-			request.Header.Add(hkey, v)
+	for key, val := range req.Header {
+		for _, v := range val {
+			request.Header.Add(key, v)
 		}
 	}
 
-	// Dump request for log information
-	data, err = httputil.DumpRequest(request, true)
-	if err != nil {
-		log.Println("httputil.DumpRequest(ext): ", err)
-	} else {
-		log.Printf("Request to ext proxy: \n%s\n", string(data))
-	}
+	mylog.WithRequest(request).Info("Forwarded request")
 
 	client, err := server.NewClient(config.ProxyAddress)
 	resp, err := client.Do(request)
 	if err != nil {
-		log.Println("client.Do: ", err)
 		http.Error(res, err.Error(), http.StatusInternalServerError)
+		log.Error(err)
 		return
 	}
 	defer resp.Body.Close()
 
-	for hkey, hval := range resp.Header {
-		for _, v := range hval {
-			res.Header().Add(hkey, v)
+	mylog.WithResponse(resp).Info("Returned response")
+
+	for key, val := range resp.Header {
+		for _, v := range val {
+			res.Header().Add(key, v)
 		}
 	}
 	res.WriteHeader(resp.StatusCode)
 
-	// Dump response for log information
-	data, err = httputil.DumpResponse(resp, false)
-	if err != nil {
-		log.Println("httputil.DumpResponse: ", err)
-	} else {
-		log.Printf("Response from ext proxy: \n%s\n", string(data))
-	}
-
 	_, err = io.Copy(res, resp.Body)
 	if err != nil {
-		log.Println(err)
 		http.Error(res, err.Error(), http.StatusInternalServerError)
+		log.Error(err)
 		return
 	}
 }
---
+++ log/util.go
@@ -0,0 +1,42 @@
+package log
+
+import (
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// WithRequest generates log entry with fields
+// from an http request informations.
+func WithRequest(req *http.Request) *log.Entry {
+	fields := log.Fields{
+		"url":      req.URL.String(),
+		"method":   req.Method,
+		"protocol": req.Proto,
+	}
+	for key, val := range req.Header {
+		for _, v := range val {
+			fields[key] = v
+		}
+	}
+	username, password, ok := req.BasicAuth()
+	if ok {
+		fields["username"] = username
+		fields["password"] = password
+	}
+	return log.WithFields(fields)
+}
+
+// WithResponse generates log entry with fields
+// from an http response informations.
+func WithResponse(res *http.Response) *log.Entry {
+	fields := log.Fields{
+		"status": res.StatusCode,
+	}
+	for key, val := range res.Header {
+		for _, v := range val {
+			fields[key] = v
+		}
+	}
+	return log.WithFields(fields)
+}
