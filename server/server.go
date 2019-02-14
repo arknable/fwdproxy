cb279eba12e27b078ff15996bbb28f8a95ae1c27
Lufty <lufty.abdillah@gmail.com>
2019-02-14 19:00:16 +0700

    relocated server config

--- server/server.go
+++ server/server.go
@@ -1,18 +1,20 @@
 package server
 
 import (
-	"crypto/tls"
 	"fmt"
 	"net/http"
 	"time"
+)
 
-	"github.com/arknable/fwdproxy/config"
+var (
+	// Port is server port
+	Port = "8000"
 )
 
 // New creates an HTTP server
 func New(handler http.Handler) *http.Server {
 	return &http.Server{
-		Addr:         fmt.Sprintf(":%s", config.HTTPPort),
+		Addr:         fmt.Sprintf(":%s", Port),
 		ReadTimeout:  20 * time.Second,
 		WriteTimeout: 20 * time.Second,
 		IdleTimeout:  120 * time.Second,
@@ -21,9 +23,9 @@ func New(handler http.Handler) *http.Server {
 }
 
 // NewTLS creates an HTTPS server
-func NewTLS(handler http.Handler) *http.Server {
-	srv := New(handler)
-	srv.Addr = fmt.Sprintf(":%s", config.TLSPort)
-	srv.TLSNextProto = make(map[string]func(*http.Server, *tls.Conn, http.Handler))
-	return srv
-}
+// func NewTLS(handler http.Handler) *http.Server {
+// 	srv := New(handler)
+// 	srv.Addr = fmt.Sprintf(":%s", config.TLSPort)
+// 	srv.TLSNextProto = make(map[string]func(*http.Server, *tls.Conn, http.Handler))
+// 	return srv
+// }
