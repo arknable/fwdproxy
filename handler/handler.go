a5a9a59173f9f970b5d475fb6152919131251bef
Lufty <lufty.abdillah@gmail.com>
2019-02-20 22:27:19 +0700

    better to check scheme

--- handler/handler.go
+++ handler/handler.go
@@ -1,12 +1,15 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // Serve is parent handler that receives request from server
 func Serve(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodConnect {
-		serveTLS(w, r)
+	if strings.ToLower(r.URL.Scheme) == "http" {
+		serveHTTP(w, r)
 		return
 	}
-	serveHTTP(w, r)
+	serveTLS(w, r)
 }
