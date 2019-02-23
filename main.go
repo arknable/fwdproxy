400d3f1b32577acb99b91bdc5833c115600367d9
Lufty <lufty.abdillah@gmail.com>
2019-02-23 17:40:31 +0700

    added log output to file

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"path"
 
 	"github.com/arknable/fwdproxy/env"
 	"github.com/arknable/fwdproxy/proxy"
@@ -12,6 +14,15 @@ func main() {
 	if err := env.Initialize(); err != nil {
 		log.Fatal(err)
 	}
+
+	// Setup log output to file and stdout
+	logPath := path.Join(env.AppHomePath(), "output.log")
+	file, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	log.SetOutput(file)
 
 	repo, err := userrepo.Initialize()
 	if err != nil {
--- proxy/handler.go
+++ proxy/handler.go
@@ -1,11 +1,14 @@
 package proxy
 
 import (
+	"log"
 	"net/http"
 )
 
 // ServeHTTP implements http.Handler
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[%s] %10s %s", r.Method, r.RemoteAddr, r.URL.String())
+
 	if r.Method != http.MethodConnect {
 		s.serveHTTP(w, r)
 		return
